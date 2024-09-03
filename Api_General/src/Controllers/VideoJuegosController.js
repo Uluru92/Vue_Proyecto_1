28faed540e9734747d345dcd9fa003e8578a55c1
Uluru92 <jorddy.castro@gmail.com>
2024-09-03 12:32:08 -0600

    update antes de continuacion

--- Api_General/src/Controllers/VideoJuegosController.js
+++ Api_General/src/Controllers/VideoJuegosController.js
@@ -10,6 +10,18 @@ const ObtenerTodosLosVideoJuegos = (req, res) =>
 const ValidarDisponibilidad = (req, res) =>
 {
     console.log('Hola', req.body);
+    
+    const informacionVideoJuego = req.body;
+    const TodosLosVideoJuegos = VideoJuegosBD.ObtenerTodosLosVideoJuegos();
+    const DisponibilidadVideoJuego = TodosLosVideoJuegos.some(x => x.disponible === informacionVideoJuego.disponible)
+
+    const RespuestaDisponibilidad = 
+    {
+        valor: DisponibilidadVideoJuego,
+        descripcion: "Hay juegos disponibles"
+    }
+    res.send(RespuestaDisponibilidad)
+
 }
 
 module.exports = {
