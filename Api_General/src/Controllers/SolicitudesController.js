ae560e7b694b66308aa64bea578b0cd667389159
Uluru92 <jorddy.castro@gmail.com>
2024-09-03 22:21:12 -0600

    solicitud ya existed updated

--- Api_General/src/Controllers/SolicitudesController.js
+++ Api_General/src/Controllers/SolicitudesController.js
@@ -1,13 +1,37 @@
 const SolicitudesBD = require("../DataBase/SolicitudesBD");
 
-const GuardarSolicitud = (req, res) => {
+const ObtenerTodasLasSolicitudes = (req, res) => {
+    const TodasLasSolicitudes = SolicitudesBD.ObtenerTodasLasSolicitudes();
+    res.send({ DetalleRespuesta: TodasLasSolicitudes })
+}
+
+const GuardarSolicitud = (req, res) =>
+{
     const NuevaSolicitud = req.body;
-    SolicitudesBD.GuardarSolicitud(NuevaSolicitud);
-    res.send("Hola esta es una nueva solicitud")
+    const TodasLasSolicitudes = SolicitudesBD.ObtenerTodasLasSolicitudes();
+    const existeSolicitud = TodasLasSolicitudes.some(x => x.NumeroTelefono === NuevaSolicitud.NumeroTelefono
+        || x.CorreoElectronico === NuevaSolicitud.CorreoElectronico)
 
+    let codigoSolicitud = 98;
+    let descripcionSolicitud = "Solicitud ya existe";
 
+    if (!existeSolicitud)
+    {
+        SolicitudesBD.GuardarSolicitud(NuevaSolicitud) 
+        codigoSolicitud = 0;
+        descripcionSolicitud = "Solicitud registrada"
+    }
+
+    const DetalleSolicitud =
+    {
+        Codigo: codigoSolicitud,
+        Descripcion: descripcionSolicitud
+    }
+
+    res.send(DetalleSolicitud)
 }
 
 module.exports = {
+    ObtenerTodasLasSolicitudes,
     GuardarSolicitud
 }
--- Api_General/src/DataBase/SolicitudesBD.js
+++ Api_General/src/DataBase/SolicitudesBD.js
@@ -2,6 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const db_archivo_directorio = path.join(__dirname, '../DataBase/SolicitudesBD.json')
 
+const ObtenerTodasLasSolicitudes = () => {
+    try {
+        let informacionSolicitudes = require(db_archivo_directorio);
+        return informacionSolicitudes.SolicitudesRegistradas;
+    }
+    catch (error) {
+        return error;
+    }
+
+}
+
 const GuardarSolicitud = (Solicitud) => {
     try {
         let informacionSolicitudes = require(db_archivo_directorio);
@@ -14,5 +25,6 @@ const GuardarSolicitud = (Solicitud) => {
 }
 
 module.exports = {
+    ObtenerTodasLasSolicitudes,
     GuardarSolicitud
 }
