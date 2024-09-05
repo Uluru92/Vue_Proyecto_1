f9f4d7d53b957f29cf207bf1b16b67c80f8cd039
Uluru92 <jorddy.castro@gmail.com>
2024-09-05 01:24:53 -0600

    exploring v model

--- proyecto_1_video_juegos/src/components/ContactoSolicitudes.vue
+++ proyecto_1_video_juegos/src/components/ContactoSolicitudes.vue
@@ -2,17 +2,17 @@
     <div style="background-color:blueviolet; margin: 2px; padding: 15px;">    
         <h1>{{Visitor}} Submit your request now!</h1>
         <div style="margin: 10px;">
-            <input type="text" v-model="userName" class="form-control" placeholder="Name">
+            <input type="text" v-model="userName" class="form-control" placeholder="User Name">
             <input type="text" v-model="PhoneNumber" class="form-control" placeholder="Phone Number">
             <input type="text" v-model="Email" class="form-control" placeholder="Email">
-            <input type="text" v-model="additionalcomment" class="form-control" placeholder="Additional Comment">
+            <input v-model="additionalcomment" placeholder="Escribe un comentario adicional">
         </div>
         <button @click="EnviarSolicitud()" class="btn btn-success">Send</button>
         <h3>{{ mensajeSolicitud1 }}</h3>
         <h3>{{ mensajeSolicitud2 }}</h3>
         <h3>{{ mensajeSolicitud3 }}</h3>
 
-        <h4 v-if="userName">Thanks for choosing us {{ userName.split('').reverse().join('')}}! Ups, thats your name backwards!!!</h4>
+        <h4 v-show="userName">Thanks for choosing us {{ userName.split('').reverse().join('')}}! Ups, thats your name backwards!!!</h4>
     </div>
 </template>
 
@@ -41,9 +41,7 @@ export default
                 userName: "",
                 PhoneNumber:"",
                 Email: "",
-                additionalcomment:""
-                
-
+                additionalcomment: "" // Asegúrate de que este campo esté correctamente definido
             }
         }
     }
