f298e7caa5ae127c5cb5c0910b37db29a0053cda
Uluru92 <jorddy.castro@gmail.com>
2024-09-04 23:44:50 -0600

    proyecto updated general

--- proyecto_1_video_juegos/src/components/ContactoSolicitudes.vue
+++ proyecto_1_video_juegos/src/components/ContactoSolicitudes.vue
@@ -1,19 +1,20 @@
 <template>
     <div style="background-color:blueviolet; margin: 2px; padding: 15px;">    
         <h1>{{Visitor}} Submit your request now!</h1>
-        <input class="form-control" placeholder="Name">
-        <input class="form-control" placeholder="Phone Number">
-        <input class="form-control" placeholder="Email">
-        <input class="form-control" placeholder="Aditional comment">
+        <div style="margin: 10px;">
+            <input class="form-control" placeholder="Name">
+            <input class="form-control" placeholder="Phone Number">
+            <input class="form-control" placeholder="Email">
+            <input class="form-control" placeholder="Aditional comment">
+        </div>
         <button @click="EnviarSolicitud()" class="btn btn-success">Send</button>
         <h3>{{ mensajeSolicitud }}</h3>
-</div>
+    </div>
 </template>
 
 <script>
 export default
     {
-
         props:
         {
             Visitor: String,
