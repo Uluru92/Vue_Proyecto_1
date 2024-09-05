15e6e5f99315258e3b0a848fe621b0c5d627d05f
Uluru92 <jorddy.castro@gmail.com>
2024-09-04 22:05:02 -0600

    implementando metodos

--- proyecto_1_video_juegos/src/components/VideoJuegos.vue
+++ proyecto_1_video_juegos/src/components/VideoJuegos.vue
@@ -1,10 +1,10 @@
 <template>
 
     <h1>Video Games!</h1>
-    <h3>Select one of our consoles:</h3>
-    <h4>{{Consola1}}</h4>
-    <h4>{{Consola2}}</h4>
-    <h4>{{Consola3}}</h4>
+    <h3>Select your favorite console:</h3>
+    <button @click="MostrarJuegosPlayStation5" style="margin: 10px; width: 120px;"><h4>{{Consola1}}</h4></button>
+    <button @click="MostrarJuegosXbox" style="margin: 10px; width: 120px;"><h4>{{Consola2}}</h4></button>
+    <button @click="MostrarJuegosNintendo" style="margin: 10px; width: 120px;"><h4>{{Consola3}}</h4></button>
 
 </template>
 
@@ -15,6 +15,19 @@ export default
             Consola1: String,
             Consola2: String,
             Consola3: String
+        },
+        methods:
+        {
+            MostrarJuegosPlayStation5() {
+                alert(`Estos son los video juegos de la consola ${this.Consola1}`)
+            },
+            MostrarJuegosXbox() {
+                alert(`Estos son los video juegos de la consola ${this.Consola2}`)
+            },
+            MostrarJuegosNintendo() {
+                alert(`Estos son los video juegos de la consola ${this.Consola3}`)
+            },
+
         }
     }
 </script>
