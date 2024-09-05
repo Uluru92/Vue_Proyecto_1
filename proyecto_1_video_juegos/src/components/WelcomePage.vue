cd5066c5b3ca0ce1cb08d54c4d677a9d0ba7e9ac
Uluru92 <jorddy.castro@gmail.com>
2024-09-04 20:18:13 -0600

    montando mi proyecto vue Uluru eSports Store

---
+++ proyecto_1_video_juegos/src/components/WelcomePage.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <h1>Uluru eSports Store</h1>
+    <h3>Welcome to Urulu Gaming!</h3>    
+    <h4> We are a small business proudly based in Costa Rica, with a physical store located in San José. We specialize in offering the best video games and related products, both in-store and through online orders.</h4>
+    <h4>We provide fast and reliable home delivery services, so you can enjoy your favorite games without leaving the comfort of your home. Our hours of operation are Monday to Friday from 11:00 a.m. to 7:00 p.m., and Saturdays from 7:00 a.m. to 12:00 p.m. At Urulu Gaming, your next adventure is just a click away!</h4>
+
+</template>
+
+<script>
+/*todo el javascript del component */
+
+    export default
+    {
+        
+    }
+
+</script>
+
+<style>
+/*Todas las etiquetas de estilo*/
+</style>
