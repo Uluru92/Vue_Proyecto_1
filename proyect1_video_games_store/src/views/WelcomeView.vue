73f48bd73eeabb151a5634c37b6adbda083d89e9
Uluru92 <jorddy.castro@gmail.com>
2024-09-15 16:36:52 -0600

    noticia 1 done

--- proyect1_video_games_store/src/views/WelcomeView.vue
+++ proyect1_video_games_store/src/views/WelcomeView.vue
@@ -3,7 +3,6 @@
       <div class="contenedorIntro">
           <h1 class="contenedorNombreStore">Uluru eSports Store</h1>
           <br>
-          <img class="contenedorImagenMonkeyKing" src="../assets/img/MonkeyKing.jpg">
           <div>
               <p class="contenedorTexto"> We are a small business proudly based in Costa Rica, with a physical store located in San José. We specialize in offering the best video games and related products, both in-store and through online orders. We provide fast and reliable home delivery services, so you can enjoy your favorite games without leaving the comfort of your home. Our hours of operation are Monday to Friday from 11:00 a.m. to 7:00 p.m., and Saturdays from 7:00 a.m. to 12:00 p.m. At Urulu Gaming, your next adventure is just a click away!</p>
           </div>
