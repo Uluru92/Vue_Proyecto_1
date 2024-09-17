96b531fa7eaa7dba8dee47b568920b075383798f
Uluru92 <jorddy.castro@gmail.com>
2024-09-16 18:37:05 -0600

    hidden mesage format

--- proyect1_video_games_store/src/views/ConsolasView.vue
+++ proyect1_video_games_store/src/views/ConsolasView.vue
@@ -7,7 +7,7 @@
             <button class="btn btn-primary" @click="MensajeJuegosPlayStation5(); habilitarMostrarMensaje()"><h4>{{Consola1}}</h4></button>
             <button class="btn btn-primary" @click="MensajeJuegosXbox(); habilitarMostrarMensaje()" ><h4>{{Consola2}}</h4></button>
             <button class="btn btn-primary" @click="MensajeJuegosNintendo(); habilitarMostrarMensaje()"><h4>{{Consola3}}</h4></button>    
-            <h6 v-if="!monstrarMensaje">{{ mensaje }}</h6>
+            <h6 class="hiddenMessage" v-if="!monstrarMensaje">{{ mensaje }}</h6>
             <h6 v-else></h6>
         </div>
     </div>
@@ -70,5 +70,14 @@
         margin: 10px; 
         width: 180px; 
         height: 60px;
+    }
+    .hiddenMessage{
+        align-items: center;
+        object-position: center;
+        margin: 10px 0px 70px;
+        padding: 10px;
+        background-color: rgb(221, 255, 0);
+        box-shadow:60px 50px 40px rgb(0, 0, 0);
+        color: rgb(0, 0, 0);
     }
 </style>
