1ab571fd813f1138c9fbb9a0a4cb2696dc85483c
Uluru92 <jorddy.castro@gmail.com>
2024-09-19 01:53:04 -0600

    comandos importantes typescript

---
+++ proyect1_video_games_store/pruebaTypeScript.ts
@@ -0,0 +1,9 @@
+/*  COMANDOS IMPORTANTES: 
+npm install - g ts - node typescript
+tsc--init
+ts - node nombre_del_archivo.ts  */
+
+let numero1: number = 999;
+let numero2: number = 111;
+let resultado: number = numero1 + numero2;
+console.log("Suma:", resultado)
